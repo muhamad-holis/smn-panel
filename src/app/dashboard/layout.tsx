ecf208d9467f452d3f52891f2b0975c31e26cf43
muhamad-holis <rainynightsuccesa@gmail.com>
2026-07-07 02:00:11 +0700

    Update layout.tsx

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -47,7 +47,7 @@ export default async function DashboardLayout({ children }: { children: React.Re
     alt="Artholic Panel" 
     width={400} 
     height={120} 
-    className="h-12 w-auto max-w-[180px] object-contain object-left"
+    className="w-[140px] h-auto object-contain"
     priority
   />
 </Link>
